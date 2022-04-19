8b9672b33b7a3bf2153d9f546a59bee1de0d9e45
puppet <puppetdev@163.com>
2022-04-19 23:13:27 +0800

    _

---
+++ src/DemoJVM.java
@@ -0,0 +1,11 @@
+/**
+ * @author puppet
+ * @since 2022/4/19 22:50
+ */
+public class DemoJVM {
+    public static void main(String[] args) {
+        String myParam = System.getProperty("myParam");
+        if (myParam == null) System.out.println("defaultParam");
+        else System.out.println(myParam);
+    }
+}
